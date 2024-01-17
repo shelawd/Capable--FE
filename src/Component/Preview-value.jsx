e283eae46589973a9a1df77a7e0492bf6b7de1be
shelawd <shelawdya44@gmail.com>
2024-01-17 13:52:51 +0700

    add: preview value

---
+++ src/Component/Preview-value.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Value = () => {
+    return (
+        <div class="value-child">
+        <h3>Your Child's Progress Report</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, perferendis?</p>
+        <div class="report">
+            <h3>Nilai Belajar: </h3>
+        <h3>Nilai Quiz: </h3>
+        <h3>Nilai Fokus: </h3>
+        </div>
+        
+
+        <a class="value-child" href="#">Kembali Ke Dashboard</a>
+    </div>
+    )
+}
+
+export default Value;
